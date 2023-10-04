c1a91dabf94348320a42e2b048d6df4331307346
Thamires Santos <106447459+thamiresantos@users.noreply.github.com>
2023-10-03 23:33:23 -0300

    concluído

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 alert('Olá Boas vindas ao jogo do número seceto');
-let numeroSecreto = 8;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+let chute ;
+let tentativas = 1;
 
-// se o chute for igual ao número secreto
-if (chute == numeroSecreto ) {
-    alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    if (chute > numeroSecreto){
-        alert(`o número secreto é menor que ${chute}`)
+// enquanto chute não for igual ao número secreto
+    //significado desse true - chute != numeroSecreto
+while (true) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    // se o chute for igual ao número secreto
+    if (chute == numeroSecreto ) {
+        break;
     } else {
-        alert(`o número secreto é maior que ${chute}`)
+        if (chute > numeroSecreto){
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`o número secreto é maior que ${chute}`);
+        }
+        tentativas++;
     }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+
+
